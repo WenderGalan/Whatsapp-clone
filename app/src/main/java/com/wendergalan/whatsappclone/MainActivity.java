3fc07434017d602c238a9145e320540cae9de8c8
Wender Galan <wendergalan2014@hotmail.com>
2017-12-27 19:25:52 -0300

    Foi adicionada as activities para navegação do cadastro e validacao do usuario via SMS!

--- app/src/main/java/com/wendergalan/whatsappclone/MainActivity.java
+++ app/src/main/java/com/wendergalan/whatsappclone/MainActivity.java
@@ -3,11 +3,22 @@ package com.wendergalan.whatsappclone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    //pega o acesso ao banco de dados
+    private DatabaseReference referenciadatabase = FirebaseDatabase.getInstance().getReference();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
